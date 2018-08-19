4c44f110efd954a370bbe311d1ad9667a6f1e7d6
Andreas Gerken <andi.gerken@gmail.com>
2018-08-19 20:17:00 +0200

    Changes on point simple to have it working with cvi

--- gym_point/envs/point_simple.py
+++ gym_point/envs/point_simple.py
@@ -7,34 +7,39 @@ from gym.utils import seeding
 import numpy as np
 
 
-class PointSimpleEnv(gym.Env):
+class PointSimpleEnv(gym.GoalEnv):
     metadata = {
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second': 50
     }
 
+    reward_range={0,1}
+    observation_range={-1,1}
+    action_range={-1,1}
+    spec = None
+
+    dimensions = 2
+    action_space = spaces.Box(
+        low=min(action_range), high=max(action_range), shape=(dimensions,), dtype=np.float32)
+    observation_space = spaces.Box(
+        low=min(observation_range), high=max(observation_range), shape=(dimensions,), dtype=np.float32)
+
+
     def __init__(self):
 
         super(PointSimpleEnv, self).__init__()
-
-        self.dimensions = 2
-
-        self.min_action = -1.0
-        self.max_action = 1.0
-        self.min_position = -1.0
-        self.max_position = 1.0
         self.max_speed = 0.05
 
         # Fixed goal to 0.5 in all dimensions. For variable goal use PointGoalEnv.
-        self.goal_position = np.array([0.] * self.dimensions)
+        self.goal = np.array([0.] * self.dimensions)
         self.goal_margin = 0.1
 
         self.viewer = None
 
-        self.action_space = spaces.Box(
-            low=self.min_action, high=self.max_action, shape=(self.dimensions,), dtype=np.float32)
-        self.observation_space = spaces.Box(
-            low=self.min_position, high=self.max_position, shape=(self.dimensions,), dtype=np.float32)
+        self.action_range = self.__class__.action_range
+        self.observation_range = self.__class__.observation_range
+        self.action_space = self.__class__.action_space
+        self.observation_space = self.__class__.observation_space
 
         if self.dimensions <= 0:
             logger.error('The dimensions have to be at least 1')
@@ -43,7 +48,6 @@ class PointSimpleEnv(gym.Env):
                 'The dimensions are bigger than 2, only the first 2 dimensions are visualized')
 
         self.seed()
-        # self.reset()
 
     def seed(self, given_seed=None):
         self.np_random, seed = seeding.np_random(given_seed)
@@ -52,7 +56,9 @@ class PointSimpleEnv(gym.Env):
         gym.spaces.np_random.seed(given_seed)
         return [seed]
 
-    def compute_reward(self, achieved_goal, desired_goal, info):
+    def compute_reward(self, achieved_goal, desired_goal = None, info= None):
+        if desired_goal is None:
+            desired_goal = self.goal
 
         distance = np.linalg.norm(achieved_goal - desired_goal)
 
@@ -61,12 +67,14 @@ class PointSimpleEnv(gym.Env):
 
         return reward, done
 
+
+
     def step(self, action):
 
         self.state += action * self.max_speed
-        self.state = np.clip(self.state, self.min_position, self.max_position)
+        self.state = np.clip(self.state, min(self.observation_range), max(self.observation_range))
 
-        reward, done = self.compute_reward(self.state, self.goal_position, {})
+        reward, done = self.compute_reward(self.state, self.goal, {})
 
         return self.state, reward, done, {}
 
@@ -101,10 +109,10 @@ class PointSimpleEnv(gym.Env):
         if self.dimensions == 1:
             # If the environment is only one dimensional, add a dimension which is 0 for rendering
             point = [self.state[0], 0]
-            goal = [self.goal_position[0], 0]
+            goal = [self.goal[0], 0]
         else:
             point = self.state
-            goal = self.goal_position
+            goal = self.goal
 
         self.pointtrans.set_translation(
             (point[0] - self.min_position) * scale, (point[1] - self.min_position) * scale)
